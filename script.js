b9707e041195e18a0832151330c5bab7a5085316
Victor Mussoline <victor8augusto010604@hotmail.com>
2023-09-14 21:10:05 -0300

    Beta 1.0 Pokedex
    First version of pokedex with html, css and js using an API

---
+++ script.js
@@ -0,0 +1,43 @@
+fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+    .then(response => response.json())
+    .then(allpokemon => {
+
+        var pokemons = [];
+
+        allpokemon.results.map((val)=>{
+
+            fetch(val.url)
+            .then(response => response.json())
+            .then(pokemonSingle => {     
+                pokemons.push({nome:val.name,imagem:pokemonSingle.sprites.front_default});
+
+                if(pokemons.length == 9){
+                    //finalizamos nossas requisições
+
+                    var pokemonBoxes = document.querySelector('.pokemon-boxes');
+                    pokemonBoxes.innerHTML = "";
+
+                    //console.log(pokemons);
+                    pokemons.map(function(val){
+                    pokemonBoxes.innerHTML+=`
+                    
+                    <div class="pokemon-box">
+                        <img src="`+val.imagem+`" />
+                        <p>`+val.nome+`</p>
+                    </div>
+
+                    `;
+                
+                        /*
+
+                        */
+                    })
+                }
+            })
+        })
+
+        pokemons.map((val)=>{
+            console.log(val.nome);
+        })
+
+    })
